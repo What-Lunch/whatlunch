d570043fa9a14d02541ac7666383d1837b5c1d5d
ramong26 <rlatndus21049@gmail.com>
2025-11-19 23:04:10 +0900

    fix: jest 이미지 무시하도록 설정 - 넥스트 이미지 절대경로만 취급하기 때문에 해당 이미지 설정이 필요

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom';
+
+jest.mock('next/image', () => {
+  const MockNextImage = props => {
+    return <img {...props} />;
+  };
+  MockNextImage.displayName = 'NextImage';
+  return MockNextImage;
+});
