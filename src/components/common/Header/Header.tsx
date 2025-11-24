dbe6338fbd359b233dc9f602a51e15ce8044e59d
ramong26 <rlatndus21049@gmail.com>
2025-11-24 20:56:30 +0900

    fix: Header 주석 변경

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -6,6 +6,7 @@ import WhatLunchLogo from '../../../../public/icons/what-lunch-logo.svg';
 /**
  * 로그인 및 회원가입 모달로 관리
  * 헤더를 학교 홈페이지처럼 하고 싶다고 했는데 아직 메뉴가 없어 하기가 어렵네요
+ * 버튼 컴포넌트가 머지되면 로그인, 회원가입도 버튼 컴포넌트로 바꿀게요
  */
 export default function Header() {
   return (
@@ -27,7 +28,6 @@ export default function Header() {
         </Link>
       </section>
 
-      {/* 버튼 컴포넌트가 아직 머지되지 않아 일단 이렇게 둘게요 */}
       <section className={styles['header__auth']}>
         <span>로그인</span>
         <span>회원가입</span>
