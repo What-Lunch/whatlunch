bf4fa4bc0a9945ed2ca72dc780f3d6f8548ec00d
ramong26 <rlatndus21049@gmail.com>
2025-12-08 19:14:29 +0900

    feature: KakaoMap - 현재 위치 마커 표시 - 위치기반 수락 필요함. 거부시 서울 시청으로 마커 표시

---
+++ src/app/test/map/page.tsx
@@ -0,0 +1,8 @@
+import KakaoMap from '@/shared/components/KakaoMap';
+export default function KakaoMapPage() {
+  return (
+    <div style={{ width: '100%', height: '100vh' }}>
+      <KakaoMap />
+    </div>
+  );
+}
--- src/shared/components/KakaoMap/KakaoMap.tsx
+++ src/shared/components/KakaoMap/KakaoMap.tsx
@@ -9,17 +9,43 @@ export default function KakaoMap() {
     const script = document.createElement('script');
     script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${KAKAOMAP}&autoload=false`;
 
+    // let markerPosition = new window.kakao.maps.LatLng(33.450701, 126.570667);
+
     script.onload = () => {
       window.kakao.maps.load(() => {
-        const mapContainer = document.getElementById('map');
-        const option = {
-          center: new window.kakao.maps.LatLng(37.5665, 126.978), // 서울 위치
-          level: 3,
-        };
-        new window.kakao.maps.Map(mapContainer, option);
+        navigator.geolocation.getCurrentPosition(
+          position => {
+            const { latitude, longitude } = position.coords;
+            const mapContainer = document.getElementById('map');
+            const option = {
+              center: new window.kakao.maps.LatLng(latitude, longitude),
+              level: 3,
+            };
+            // 현재 위치 마커 표시
+            const map = new window.kakao.maps.Map(mapContainer, option);
+            const markerPosition = new window.kakao.maps.LatLng(latitude, longitude);
+            const marker = new window.kakao.maps.Marker({
+              position: markerPosition,
+            });
+            marker.setMap(map);
+          },
+          () => {
+            const mapContainer = document.getElementById('map');
+            const option = {
+              center: new window.kakao.maps.LatLng(37.5665, 126.978), // 서울 시청 위치
+              level: 3,
+            };
+            const map = new window.kakao.maps.Map(mapContainer, option);
+            const markerPosition = new window.kakao.maps.LatLng(37.5665, 126.978);
+            const marker = new window.kakao.maps.Marker({
+              position: markerPosition,
+            });
+            marker.setMap(map);
+          }
+        );
       });
     };
     document.head.appendChild(script);
   }, [KAKAOMAP]);
-  return <div id="map" style={{ width: '100%', height: '400px' }} />;
+  return <div id="map" style={{ width: '100%', height: '100%' }} />;
 }
