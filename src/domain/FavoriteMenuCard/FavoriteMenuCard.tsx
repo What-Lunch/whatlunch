14d5e92929f2af20579d2ffd77fc819c44aec9d1
ramong26 <rlatndus21049@gmail.com>
2026-01-04 10:57:05 +0900

    feature: FavoriteMenuCard - 스타일 base완료

--- src/domain/FavoriteMenuCard/FavoriteMenuCard.tsx
+++ src/domain/FavoriteMenuCard/FavoriteMenuCard.tsx
@@ -1,28 +1,80 @@
 'use client';
 import { useState } from 'react';
+import { TrophyIcon } from 'lucide-react';
 
 import Modal from '@/shared/components/Modal';
+
+import { FAVORITE_MEAL_MOCK } from './mock';
+
+import styles from './FavoriteMenuCard.module.scss';
 
 export default function FavoriteMenuCard() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   return (
-    <div>
-      <section>
-        <div>Favorite Menu Card Content</div>
-        <button onClick={() => setIsModalOpen(true)}>Open Modal</button>
+    <>
+      <section aria-label="즐겨찾기 메뉴" className={styles['meal-favorite']}>
+        <header className={styles['meal-favorite__header']}>
+          <h2 className={styles['meal-favorite__header__title']}>자주 선택된 메뉴 요약</h2>
+          <TrophyIcon className={styles['meal-favorite__header__icon']} />
+        </header>
+        {FAVORITE_MEAL_MOCK.length === 0 ? (
+          <div className={styles['meal-favorite__empty']}>
+            <TrophyIcon className={styles['meal-favorite__empty__icon']} />
+            <p className={styles['meal-favorite__empty__text']}>아직 즐겨찾기 메뉴가 없어요!</p>
+          </div>
+        ) : (
+          <>
+            <ul className={styles['meal-favorite__list']}>
+              {FAVORITE_MEAL_MOCK.slice(0, 3).map(item => (
+                <li key={item.id} className={styles['meal-favorite__list__item']}>
+                  {/* TODO: 각 음식 사진이나 아이콘 추가 필요 */}
+                  <div className={styles['meal-favorite__list__item__info']}>
+                    <span className={styles['meal-favorite__list__item__info__name']}>
+                      {item.menuName}
+                    </span>
+                    <span className={styles['meal-favorite__list__item__info__category']}>
+                      {item.category}
+                    </span>
+                  </div>
+                  <span>{item.count}회</span>
+                </li>
+              ))}
+            </ul>
+            <div className={styles['meal-favorite__view-all']}>
+              <button
+                className={styles['meal-favorite__view-all__button']}
+                onClick={() => setIsModalOpen(true)}
+              >
+                전체기록 보기
+              </button>
+            </div>
+          </>
+        )}
       </section>
       {isModalOpen && (
         <>
           <Modal
             isOpen={isModalOpen}
             onClose={() => setIsModalOpen(false)}
-            title="Favorite Menu"
-            description="설명설명"
+            title="즐겨찾기 메뉴 전체 기록"
+            innerClassName={styles['modal']}
           >
-            <>ㅎㅇㅎㅇ</>
+            <ul className={styles['modal__list']}>
+              {FAVORITE_MEAL_MOCK.map(item => (
+                <li key={item.id} className={styles['modal__list__item']}>
+                  <div className={styles['modal__list__item__info']}>
+                    <span className={styles['modal__list__item__info__name']}>{item.menuName}</span>
+                    <span className={styles['modal__list__item__info__category']}>
+                      {item.category}
+                    </span>
+                  </div>
+                  <span>{item.count}회</span>
+                </li>
+              ))}
+            </ul>
           </Modal>
         </>
       )}
-    </div>
+    </>
   );
 }
