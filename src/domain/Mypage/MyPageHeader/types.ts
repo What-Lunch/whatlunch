1c7e939d079d7f15d6f7352d404ec751e0b995a9
ramong26 <rlatndus21049@gmail.com>
2026-01-14 20:12:47 +0900

    chore: MyPageHeader - 필요없는 코드 삭제

--- src/domain/Mypage/MyPageHeader/types.ts
+++ src/domain/Mypage/MyPageHeader/types.ts
@@ -1,16 +1,5 @@
-// import type { LucideIcon } from 'lucide-react';
-
 export interface UserProfile {
   id: string;
   nickname: string;
   profileImageUrl: string;
 }
-
-// export type BadgeVariant = 'blue' | 'green' | 'purple' | 'orange';
-
-// export interface MyPageBadge {
-//   id: string;
-//   variant: BadgeVariant;
-//   Icon: LucideIcon;
-//   text: string;
-// }
