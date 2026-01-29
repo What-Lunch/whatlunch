e7bbf4214182c297807e07261d5d520df4cacc6a
ramong26 <rlatndus21049@gmail.com>
2026-01-29 13:36:34 +0900

    fix: getApiBaseUrl - 에러 메세지 추가 및 클라이언트에서도 안전하게 호츌

--- src/shared/hooks/getApiBaseUrl.ts
+++ src/shared/hooks/getApiBaseUrl.ts
@@ -1,8 +1,12 @@
 export const getApiBaseUrl = (): string => {
-  return (
-    process.env.NEXT_PUBLIC_API_BASE_URL ||
-    process.env.NEXT_PUBLIC_API_URL ||
-    process.env.BASE_URL ||
-    'http://localhost:8080'
-  );
+  const url =
+    process.env.NEXT_PUBLIC_API_BASE_URL || process.env.NEXT_PUBLIC_API_URL || process.env.BASE_URL;
+
+  if (!url) {
+    if (process.env.NODE_ENV === 'production') {
+      throw new Error('[API] API base URL environment variable is not defined.');
+    }
+    return 'http://localhost:8080';
+  }
+  return url;
 };
